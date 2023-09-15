1911759fea89cab9377f0fe12cca8460cf94fb6a
Stefeson <e107ti@etec.sp.gov.br>
2023-09-15 20:32:50 -0300

    Decomposição Decimal

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿// Stefeson
+
+Console.Clear();
+
+int numero, cen, dez, uni;
+
+Console.Write("Coloque algum número: ");
+numero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"O número que você digitou contém:");
+
+cen = numero / 100;
+dez = (numero % 100) / 10;
+uni = (numero % 100) % 10;
+
+Thread.Sleep(2000);
+Console.WriteLine($"{uni} Unidade(s)");
+Thread.Sleep(1000);
+Console.WriteLine($"{dez} Dezenas(s)");
+Thread.Sleep(1000);
+Console.WriteLine($"{cen} Centenas(s)");
